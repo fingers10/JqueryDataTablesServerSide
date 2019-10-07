19eff46293c5050eadd445e1caa235fd6217ed27
fingers10 <abdulrahman.smsi@gmail.com>
2019-10-07 19:58:14 +0530

    added release notes

--- AspNetCoreWeb/Reports/JqueryDataTablesReportProcessor.cs
+++ AspNetCoreWeb/Reports/JqueryDataTablesReportProcessor.cs
@@ -48,31 +48,22 @@ namespace JqueryDataTables.ServerSide.AspNetCoreWeb.Reports
             //https://stackoverflow.com/questions/564366/convert-generic-list-enumerable-to-datatable
             // To restrict order or specific property
             //ObjectReader.Create(data, "Id", "Name", "Description")
-            using (var reader = ObjectReader.Create(data))
-            {
-                var table = new DataTable(name ?? typeof(T).Name);
-                table.Load(reader);
-                return table;
-            }
+            using var reader = ObjectReader.Create(data);
+            var table = new DataTable(name ?? typeof(T).Name);
+            table.Load(reader);
+            return table;
         }
 
         public static async Task<byte[]> GenerateExcelForDataTableAsync<T>(this IEnumerable<T> data, string name)
         {
             var table = await data.ToDataTableAsync(name);
 
-            using (var wb = new XLWorkbook(XLEventTracking.Disabled))
-            {
-                wb.Worksheets
-                    .Add(table)
-                    .ColumnsUsed()
-                    .AdjustToContents();
+            using var wb = new XLWorkbook(XLEventTracking.Disabled);
+            wb.Worksheets.Add(table).ColumnsUsed().AdjustToContents();
 
-                using (var stream = new MemoryStream())
-                {
-                    wb.SaveAs(stream);
-                    return stream.ToArray();
-                }
-            }
+            using var stream = new MemoryStream();
+            wb.SaveAs(stream);
+            return stream.ToArray();
         }
     }
 }
