fb45f844f2658fcfe1e587917d560b053beef67e
fingers10 <abdulrahman.smsi@gmail.com>
2020-04-19 16:03:57 +0530

    Fixed #36 - Added support for CSV export and updated release notes.

---
+++ AspNetCoreWeb/ActionResults/JqueryDataTablesCSVResult.cs
@@ -0,0 +1,12 @@
+﻿using Fingers10.ExcelExport.ActionResults;
+using System.Collections.Generic;
+
+namespace JqueryDataTables.ServerSide.AspNetCoreWeb.ActionResults
+{
+    public class JqueryDataTablesCSVResult<T> : CSVResult<T> where T : class
+    {
+        public JqueryDataTablesCSVResult(IEnumerable<T> data, string fileName) : base(data, fileName)
+        {
+        }
+    }
+}
