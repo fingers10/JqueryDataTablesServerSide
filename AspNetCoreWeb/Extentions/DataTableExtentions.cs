5cc7dcf043fe54683e75bc35ef1424573385deb3
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2020-07-01 20:00:52 +0500

    added Extention Methods to convert data table result from list

---
+++ AspNetCoreWeb/Extentions/DataTableExtentions.cs
@@ -0,0 +1,43 @@
+﻿using JqueryDataTables.ServerSide.AspNetCoreWeb.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JqueryDataTables.ServerSide.AspNetCoreWeb.Extentions
+{
+    /// <summary>
+    /// Extention Methods to convert data table result from list
+    /// </summary>
+    public static class DataTableExtentions
+    {
+        public static JqueryDataTablesResult<T> ToDataTable<T>(this IEnumerable<T> list, int draw, int totalCount, int filteredCount)
+        {
+            return new JqueryDataTablesResult<T>
+            {
+                Data = list,
+                Draw = draw,
+                RecordsFiltered = filteredCount,
+                RecordsTotal = totalCount
+            };
+        }
+        public static JqueryDataTablesResult<T> ToDataTable<T>(this IEnumerable<T> list, int draw, int totalCount)
+        {
+            return new JqueryDataTablesResult<T>
+            {
+                Data = list,
+                Draw = draw,
+                RecordsFiltered = list.Count(),
+                RecordsTotal = totalCount
+            };
+        }
+        public static JqueryDataTablesResult<T> ToDataTable<T>(this IEnumerable<T> list, int draw)
+        {
+            return new JqueryDataTablesResult<T>
+            {
+                Data = list,
+                Draw = draw,
+                RecordsFiltered = list.Count(),
+                RecordsTotal = list.Count()
+            };
+        }
+    }
+}
